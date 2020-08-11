7e3dd4a2eb6ffc3dee80915938d851c7f70ef4a6
Ralph Mallett <rmallett12@gmail.com>
2020-08-11 15:13:20 +0100

    Amend views to .erb format, made routes restful, added to app.rb

--- app.rb
+++ app.rb
@@ -5,5 +5,17 @@ class MakersBnb < Sinatra::Base
     'Welcome to MakersBnb'
   end
 
+  get '/spaces' do
+    erb :'spaces/view_spaces'
+  end
+
+  get '/spaces/new' do
+    erb :'spaces/new/add_space'
+  end
+
+  post '/spaces/new' do
+    erb :'spaces/new/add_space'
+  end
+
   run! if app_file == $0
 end
