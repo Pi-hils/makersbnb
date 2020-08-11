fdb121197aff0277bfca34f8f523dd1d372a0d9c
Mark <markjpoole@yahoo.com>
2020-08-11 16:22:57 +0100

    add spaces routes to app.rb

--- app.rb
+++ app.rb
@@ -12,6 +12,20 @@ class MakersBnb < Sinatra::Base
 
   get '/' do
     'Welcome to MakersBnb'
+    erb(:index)
   end
+
+  post '/spaces/add' do
+    Spaces.add(name: params[:space_name], price: params[:price], description: params[:description],
+      availability_start: params[:availability_start], availability_end: params[:availability_end],
+      host_id: params[:host_id])
+    erb(:index)
+  end
+
+  get '/spaces' do
+    @spaces = Spaces.all
+    erb(:index)
+  end
+
 
 end
