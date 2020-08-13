f84afe2240fcce5b21a746a3cac46b13c9523e20
Daniel M <danielim@hotmail.co.uk>
2020-08-13 22:22:49 +0100

    bug fixes, adding front-end functionality and back-end features

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ class MakersBnb < Sinatra::Base
   end
 
   get '/dashboard' do
-    @user = User.find(id: session[:user_id])
+    p @user = User.find(id: session[:user_id])
     erb :dashboard
   end
 
@@ -43,30 +43,34 @@ class MakersBnb < Sinatra::Base
 
   get '/your_hostings' do
     # @requests = Request.get_requests(params[:host_id])
-    @user = User.find(id: session[:user_id])
+    p @user = User.find(id: session[:user_id])
     @requests = Request.get_requests(host_id: session[:user_id])
     erb :your_hostings
   end
 
   get '/your_stays' do
-    @user = User.find(id: session[:user_id])
+    p @user = User.find(id: session[:user_id])
+    @stays = Request.get_stays(guest_id: session[:user_id])
     erb :your_stays
   end
 
   get '/:space_id/space_details' do
     @space = Spaces.find(params[:space_id])
+    p @user = User.find(id: session[:user_id])
     erb :space_details
   end
 
   get '/' do
-    erb(:login) # Needs to be resolved to an actual route
+    erb(:login)
   end
 
-  get '/spaces/add' do
+  get '/spaces/new' do
+    p @user = User.find(id: session[:user_id])
     erb :add_space
   end
 
-  post '/spaces/:host_id/add' do
+  post '/spaces/:host_id/new' do
+    p params
     Spaces.add(name: params[:space_name], price: params[:price], description: params[:description],
       availability_start: params[:availability_start], availability_end: params[:availability_end],
       host_id: params[:host_id])
@@ -74,6 +78,7 @@ class MakersBnb < Sinatra::Base
   end
 
   get '/spaces' do
+    p @user = User.find(id: session[:user_id])
     @spaces = Spaces.all
     erb(:view_spaces)
   end
