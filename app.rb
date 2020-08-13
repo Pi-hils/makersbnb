dd9a513cd77499f6e916eafb774f4a7a0c805f57
Hilda Amponsah <hilda.amp@hotmail.com>
2020-08-13 01:23:25 +0100

    added GET method for spaces/add and a few changes in views

--- app.rb
+++ app.rb
@@ -39,15 +39,23 @@ class MakersBnb < Sinatra::Base
     erb :'space_details'
   end
 
+  get '/spaces/add' do
+    p 'Hello'
+    erb :'add_space'
+  end
+
   post '/spaces/add' do
+    p 'Hello World'
     Spaces.add(name: params[:space_name], price: params[:price], description: params[:description],
       availability_start: params[:availability_start], availability_end: params[:availability_end],
       host_id: params[:host_id])
-    erb(:index)
+      # needs to be removed as already created a route method GET for this and index doesn't exist
+    erb (:index)
   end
 
   get '/spaces' do
     @spaces = Spaces.all
+     # there is no 'index.erb'..yet
     erb(:index)
   end
 
